8afc0b0525846dea089a190736e42de805df5aca
Renato Moutinho Silva <renato.moutinho@gmail.com>
2020-07-19 00:14:49 -0300

    appgobarber first commit

---
+++ appgobarber/src/Pages/SignIn/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Image } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="email" />
+      <Input name="password" icon="lock" placeholder="senha" />
+      <Button
+        onPress={() => {
+          console.log('Entrar');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
