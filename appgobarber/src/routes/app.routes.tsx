c82b94b98dddaa7eaf7b8b17e78f8a0538f92971
Renato Moutinho Silva <renato.moutinho@gmail.com>
2020-07-20 10:41:11 -0300

    Lição 3 concluída

--- appgobarber/src/routes/app.routes.tsx
+++ appgobarber/src/routes/app.routes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Dashboard from '../Pages/dashboard';
+import Dashboard from '../Pages/Dashboard';
 
 const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
-      // headerShown: false,
+      headerShown: false,
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
